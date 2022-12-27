5ce9ed7051f0d2d33f6d960bc817f36347863a08
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-12-28 03:13:13 +0900

    ㅁ

--- src/components/main/Complete.jsx
+++ src/components/main/Complete.jsx
@@ -3,11 +3,16 @@ import { ToDoContext } from "../../context/ToDoContext";
 import main from "../../css/main.module.css";
 
 export default function Complete({ id }) {
-  const { completeItem } = useContext(ToDoContext);
+  const { list, completeItem } = useContext(ToDoContext);
   const handleClick = (e) => {
     completeItem(id);
   };
   return (
-    <input type="checkbox" className={main.checkbox} onClick={handleClick} />
+    <input
+      type="checkbox"
+      className={main.checkbox}
+      onClick={handleClick}
+      checked={list.find((item) => item.id === id).complete ? true : false}
+    />
   );
 }
