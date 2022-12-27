66430913a96101e7c9c63668c6b6be2cbf2fed9d
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-12-27 16:19:51 +0900

    Button.jsx : 버튼 클릭 기능 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../css/header.module.css";
 
+/**
+ * 버튼은 클릭하면 액티브되는 css 기능과 필터링하는 기능을 한다.
+ * @param {*} param0
+ * @returns
+ */
 export default function Button({ name }) {
-  // 필터링 되는 기능
-  return <div className={styles.button}>{name}</div>;
+  const [isActive, setIsActive] = useState(false);
+
+  /**
+   * 1. 클릭 => active
+   */
+  const handleClick = () => {
+    setIsActive((prev) => !prev);
+  };
+  return (
+    <div
+      className={`${styles.button} ${styles[isActive]}`}
+      onClick={handleClick}
+    >
+      {name}
+    </div>
+  );
 }
