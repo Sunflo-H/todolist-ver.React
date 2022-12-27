ada1cb550877747691d0a7512226f0ca4a32ce49
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-12-28 03:12:59 +0900

    Complete.jsx : 필터를 해도 체크상태가 유지되게끔 코드 수정

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -10,8 +10,8 @@ import styles from "./css/index.module.css";
 export default function ToDoList() {
   /**
    * 1. 다크모드 라이트모드 + => 호버하면 반을 바꾼다. 클릭하면 나머지 반도 바꾼다.
-   * 2. 필터링 - ALl, Active, Completed
-   * 3. 추가 삭제
+   * 2. 필터링 - ALl, Active, Completed +
+   * 3. 추가 삭제 +
    * 4. 완료한거 체크
    * 5. 로컬스토리지에 데이터 저장 => 12시가 지나면 초기화
    * 6. 폰트색
