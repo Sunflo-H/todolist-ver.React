c99307cfe1f02c943f25fb1d03a35ef91e486e85
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-12-29 01:39:03 +0900

    color 로컬저장소에 저장

--- src/context/ColorContext.jsx
+++ src/context/ColorContext.jsx
@@ -1,12 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ColorContext = createContext();
 
 export function ColorProvider({ children }) {
   const [color, setColor] = useState(false);
   const changeColor = () => {
-    setColor((prev) => !prev);
+    setColor((prev) => {
+      let current = !prev;
+      localStorage.setItem("color", JSON.stringify(current));
+
+      return !prev;
+    });
   };
+
+  const getColorFromLocalStorage = () => {
+    let result = color;
+    if (localStorage.getItem("color")) {
+      result = JSON.parse(localStorage.getItem("color"));
+    }
+    return result;
+  };
+
+  useEffect(() => {
+    setColor(getColorFromLocalStorage());
+  }, []);
+
   return (
     <ColorContext.Provider value={{ color, changeColor }}>
       {children}
